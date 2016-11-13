cdd6f127258ee1aacd694c6bcf7d9542d3dd5b2e
Matteo Rigotti <matte.rigo@gmail.com>
2016-11-14 00:26:35 +0100

    pulizia css e sourceComments

--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,30 +1,14 @@
 var webpack = require('webpack');
-var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var plugins = [];
 
-var production = false;
-
-if (production) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  );
-}
-
 plugins.push(
-  new ExtractTextPlugin(
-    path.join(
-      '..', 'css', 'theme.css'
-    )
-  )
+  new ExtractTextPlugin('../css/theme.css')
 );
 
-module.exports = {
+module.exports = [{
+  // JavaScript
   entry: [
     './js/theme.js'
   ],
@@ -37,29 +21,37 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loaders: ['babel-loader']
-    }, {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract(
-        "style",
-        "css?sourceMap!postcss!sass?sourceMap"
-      )
-    }, {
-      test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-      loader: 'file-loader?name=../css/[hash].[ext]'
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader!postcss-loader"
     }]
-  },
-  postcss: function() {
-    return [require('postcss-flexibility')];
   },
   externals: {
     prestashop: 'prestashop'
   },
-  devtool: 'source-map',
   plugins: plugins,
   resolve: {
-    extensions: ['', '.js', '.scss']
+    extensions: ['', '.js']
   }
-};
+}, {
+  // CSS
+  entry: [
+    './css/theme.scss'
+  ],
+  output: {
+    path: '../assets/js',
+    filename: 'theme.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract(
+        "style",
+      )
+    }, {
+      test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+      loader: 'file-loader?name=../css/[hash].[ext]'
+    }]
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['', '.scss', '.styl', '.less', '.css']
+  }
+}];
